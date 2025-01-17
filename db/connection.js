42b7e47b7f54ff5778d5fa5dd58dbbd3e91a5f6d
javiBarrientos <javibarrientosfernandez99@gmail.com>
2025-01-17 10:48:07 +0100

    Make database connection

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+import { MongoClient } from "mongodb";
+import "../loadEnvironment.js";
+
+const connectionString = process.env.DATABASE_URL || "";
+const client = new MongoClient(connectionString);
+
+let connection;
+
+try {
+    connection = await client.connect();
+} catch (e) {
+    console.error(e)
+}
+
+let database = connection.db('blog')
+export default database;
